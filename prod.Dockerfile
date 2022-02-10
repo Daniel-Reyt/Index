389233c8e261f843f5d9757cb0fcdb6dba90b51e
Reyt Daniel-David <daniel-david.reyt@uha.fr>
2022-02-10 09:29:48 +0100

    initial

---
+++ prod.Dockerfile
@@ -0,0 +1,20 @@
+FROM node as builder
+
+RUN mkdir -p /app
+RUN chmod -R 777 /app
+
+WORKDIR /app
+
+ENV PATH /app/node_modules/.bin:$PATH
+
+COPY . .
+RUN npm install
+
+COPY . .
+
+FROM nginx:stable-alpine
+COPY --from=builder /app/dist/front /usr/share/nginx/html
+RUN rm /etc/nginx/conf.d/default.conf
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IndexServer';
+
+  webserver = [
+    {
+      name: 'jurilog',
+      status: 'ON',
+      url: 'http://51.15.2.21:300'
+    }
+  ]
+
+  gameserver = [
+    {
+      text: 'Serveur Minecraft Spigot 1.18.1 avec plugins',
+      ip: '51.15.2.21:25567',
+      whitelist: 'OFF',
+      type: 'serveur survie tranquille entre potes',
+      status: 'ON'
+    }, 
+    {
+      text: 'Serveur Minecraft Manhunt 1.18.1',
+      ip: '51.15.2.21:25566',
+      whitelist: 'OFF',
+      type: 'serveur event ManHunt',
+      status: 'ON'
+    }, 
+    {
+      text: 'Serveur Minecraft BuildBattle 1.18.1',
+      ip: '51.15.2.21:25565',
+      whitelist: 'OFF',
+      type: 'serveur event BuildBattle',
+      status: 'ON'
+    }
+  ]
 }
